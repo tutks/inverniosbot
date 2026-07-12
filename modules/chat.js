a279199c27601c38bb802963803f9d5c5a28cf3d
tutks <pruebasnosea@gmail.com>
2026-07-12 15:38:42 -0300

    bla bla bla ble ble ble blu blu blu

--- modules/chat.js
+++ modules/chat.js
@@ -1,7 +1,6 @@
 const config = require("../config.json");
 
 let chatTimer = null;
-let loginTime = Date.now();
 
 const recentMessages = [];
 const MAX_RECENT_MESSAGES = 3;
@@ -11,9 +10,6 @@ function initialize(bot) {
     if (!config.chat.enabled) return;
 
     bot.once("spawn", () => {
-
-        loginTime = Date.now();
-
         schedule(bot);
 
     });
@@ -64,9 +60,7 @@ function sendMessage(bot) {
         Math.floor(Math.random() * availableMessages.length)
     ];
 
-    const finalMessage = replaceVariables(bot, message);
-
-    bot.chat(finalMessage);
+    bot.chat(message);
 
     recentMessages.push(message);
 
@@ -75,86 +69,6 @@ function sendMessage(bot) {
     }
 
     schedule(bot);
-
-}
-
-function replaceVariables(bot, message) {
-
-    const onlinePlayers = Object.keys(bot.players).length;
-
-    const playerNames = Object.keys(bot.players)
-        .filter(name => name !== bot.username);
-
-    const randomPlayer =
-        playerNames.length > 0
-            ? playerNames[Math.floor(Math.random() * playerNames.length)]
-            : "nadie";
-
-    const hours = Math.floor((Date.now() - loginTime) / 3600000);
-    const minutes = Math.floor(((Date.now() - loginTime) % 3600000) / 60000);
-
-    message = message.replaceAll("{player}", randomPlayer);
-
-    message = message.replaceAll("{players}", onlinePlayers);
-
-    message = message.replaceAll("{time}", `${hours}h ${minutes}m`);
-
-    message = message.replaceAll("{health}", Math.floor(bot.health));
-
-    message = message.replaceAll("{food}", Math.floor(bot.food));
-
-    // Día del mundo (si Mineflayer lo expone)
-    if (bot.time && typeof bot.time.age === "number") {
-        const worldDay = Math.floor(bot.time.age / 24000) + 1;
-        message = message.replaceAll("{day}", worldDay);
-    }
-
-    // Día o noche
-    if (bot.time) {
-
-        let isDay = "desconocido";
-
-        if (typeof bot.time.isDay === "boolean") {
-            isDay = bot.time.isDay ? "día" : "noche";
-        } else if (typeof bot.time.timeOfDay === "number") {
-            isDay = (bot.time.timeOfDay >= 0 && bot.time.timeOfDay < 12000)
-                ? "día"
-                : "noche";
-        }
-
-        message = message.replaceAll("{daytime}", isDay);
-
-    }
-
-    // Clima
-    if (typeof bot.isRaining === "boolean") {
-        message = message.replaceAll(
-            "{weather}",
-            bot.isRaining ? "lloviendo" : "despejado"
-        );
-    }
-
-    // Bioma
-    if (bot.entity && bot.entity.position) {
-
-        try {
-
-            const biome = bot.world.getBiome(bot.entity.position);
-
-            if (biome && biome.name) {
-                message = message.replaceAll("{biome}", biome.name);
-            }
-
-        } catch {}
-
-    }
-
-    // Dimensión
-    if (bot.game && bot.game.dimension) {
-        message = message.replaceAll("{dimension}", bot.game.dimension);
-    }
-
-    return message;
 
 }
 

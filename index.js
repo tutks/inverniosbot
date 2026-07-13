fc88b88a2bc65ccdcf6d6865e863055e1ca451c1
tutks <pruebasnosea@gmail.com>
2026-07-13 00:00:00 -0300

    agregar cosit cosut

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const botmanager = require("./modules/botmanager");
 
 botmanager.start();
+
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Bot de minecraf activo");
+});
+
+app.listen(PORT, () => {
+    console.log('servidor web escuchando en el puerto ${PORT}');
+});
